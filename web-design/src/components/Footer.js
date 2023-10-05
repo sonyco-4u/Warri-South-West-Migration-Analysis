008adaac8d880ba87caab56ec6af2ac89f71f5b2
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-10-05 09:14:00 -0700

    Add 2 web pages

---
+++ web-design/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.js';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>&copy; 2023 Copyright. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ web-design/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.js';
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1>Welcome to Evoteck Geospatial Consult</h1>
+    </header>
+  );
+}
+
+export default Header;
